349b1c1c0309622c089c4dc66ae725d7ce659656
Gena <daogena@gmail.com>
2020-08-25 20:55:42 -0500

    Added body, header, player, and song row

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from "./Login";
+import Player from "./Player";
+import { getTokenFromUrl } from './spotify'; 
+import SpotifyWebApi from "spotify-web-api-js";
+import { useDataLayerValue } from "./DataLayer";  
+
+const spotify = new SpotifyWebApi(); 
 
 function App() {
+  const [{ user, token }, dispatch] = useDataLayerValue(); 
+
+  useEffect(() => {
+    const hash = getTokenFromUrl(); 
+    window.location.hash = ""; 
+    const _token = hash.access_token; 
+
+    if (_token) {
+
+      dispatch({
+        type: "SET_TOKEN", 
+        token: _token, 
+      });
+      
+      // Giving access token to spotify
+      spotify.setAccessToken(_token); 
+
+      spotify.getMe().then(user => {
+        dispatch({
+          type: 'SET_USER', 
+          user: user
+        }); 
+      }); 
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS", 
+          playlists: playlists, 
+        }); 
+      }); 
+
+      spotify.getPlaylist("37i9dQZEVXcXnBUHi1cNpQ").then((response) => 
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY", 
+          discover_weekly: response, 
+        })
+      );
+
+    }
+
+  }, [])
+
   return (
     <div className="app">
-      <h1>Hello</h1>
+      {
+        // If there's a token, render Player
+        // Otherwise, render Login component
+        token ? (
+          <Player spotify={spotify} />
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
